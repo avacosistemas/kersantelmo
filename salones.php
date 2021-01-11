1c9eb58aaf79bf2be0ac8591235c9a2dd7626266
gonzalooluna <gonzaloo.luna@hotmail.com>
2021-01-11 18:16:07 -0300

    se llama desde un inlude al archivo mensaje_whatsapp

--- salones.php
+++ salones.php
@@ -84,15 +84,7 @@ Formato cocktail, banquete con o sin pista de baile, auditorio, son algunas de l
 		       </div>
 	       </div>
 					  
-<!--  SE AGREGA BOTON DE WHATSAPP   -->
-
-<div >
-        <a href="https://api.whatsapp.com/send?phone=5491130174497&text=Hola!%20Me%20contacto%20a%20través%20de%20la%20web%20y%20me%20gustaría%20realizar%20una%20consulta" target="_blank">
-            <img class="btn-whatsapp" src="img/icons/icon-wsp.png" alt="">
-        </a>
-    </div>
-
-<!--  SE AGREGA BOTON DE WHATSAPP   -->        
+		   <?php include("mensaje_whatsapp.php");?>
         <?php include("footer.php");?>
 <script src="js/jquery.bxslider1/jquery.bxslider.js"></script>
 		<link rel="stylesheet" href="js/jquery.bxslider1/jquery.bxslider.css">
